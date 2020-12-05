a5e411cf0373141baa5fdc0d3f4a76d4a8116c2b
PureFunctor <purefunctor@gmail.com>
2020-12-05 11:36:59 +0800

    Add basic routing example

--- app/Main.hs
+++ app/Main.hs
@@ -8,12 +8,20 @@ data HelloWorld = HelloWorld
 
 mkYesod "HelloWorld" [parseRoutes|
 / HomeR GET
+/about AboutR GET
+/info InfoR GET
 |]
 
 instance Yesod HelloWorld
 
 getHomeR :: Handler Html
-getHomeR = defaultLayout [whamlet|Hello World!|]
+getHomeR = defaultLayout [whamlet|<a href=@{AboutR}>About Page!</a><a href=@{InfoR}>Info Page!</a>|]
+
+getAboutR :: Handler Html
+getAboutR = defaultLayout [whamlet|About Page!|]
+
+getInfoR :: Handler Html
+getInfoR = defaultLayout [whamlet|Info Page!|]
 
 main :: IO ()
 main = warp 3000 HelloWorld
